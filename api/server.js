9302dd9f192839987b6fb81169700c2165e6bead
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-25 19:51:36 +0100

    wip

--- api/server.js
+++ api/server.js
@@ -12,7 +12,16 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 server.use(session({
-  // huge
+  name: "monkey", // the default would be sid, but that would reveal our stack
+  secret: "keep it secret, keep it safe!", // to encrypt/decrypt the cookie
+  cookie: {
+    maxAge: 1000 * 60 * 60, // how long is the session valid for, in milliseconds
+    secure: false, // used over https only, should be true in production
+    httpOnly: true, // cannot access the cookie from JS using document.cookie
+    // keep this true unless there is a good reason to let JS access the cookie
+  },
+  resave: false, // we might need to set this to true to avoid idle sessions being deleted
+  saveUninitialized: false, // keep it false to avoid storing sessions and sending cookies for unmodified sessions
 }))
 
 server.use("/api/users", usersRouter);
