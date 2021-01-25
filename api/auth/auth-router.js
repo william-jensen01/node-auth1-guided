5cbe39c3c1752cb403db6c791cfbe314da511603
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-25 18:52:50 +0100

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("../users/users-model.js");
+const bcrypt = require("bcryptjs");
 const router = express.Router();
 
 router.post('/register', (req, res) => {
