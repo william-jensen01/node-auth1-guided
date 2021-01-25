3950fa6a961670f491800ff80d83018cb0fcf365
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-25 18:46:33 +0100

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,17 +1,17 @@
 const express = require("express");
-const User = require("../users/users-model.js")
-const router = express.Router()
+const User = require("../users/users-model.js");
+const router = express.Router();
 
 router.post('/register', (req, res) => {
-  res.json('silly register')
-})
+  res.json('silly register');
+});
 
 router.post('/login', (req, res) => {
-  res.json('silly login')
-})
+  res.json('silly login');
+});
 
 router.get('/logout', (req, res) => {
-  res.json('silly logout')
-})
+  res.json('silly logout');
+});
 
-module.exports = router
+module.exports = router;
