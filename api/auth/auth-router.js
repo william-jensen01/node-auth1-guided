ed6084fbe888600cc8691cf18fd6124ebbc6056d
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-25 18:43:20 +0100

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -6,7 +6,11 @@ router.post('/register', (req, res) => {
 
 })
 
-router.post('/register', (req, res) => {
+router.post('/login', (req, res) => {
+
+})
+
+router.get('/logout', (req, res) => {
 
 })
 
