e72e40b5e4aee1a1653164fda2a7e7ca6ba449e3
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-25 20:17:27 +0100

    wip

--- api/server.js
+++ api/server.js
@@ -15,7 +15,7 @@ server.use(session({
   name: "monkey", // the default would be sid, but that would reveal our stack
   secret: "keep it secret, keep it safe!", // to encrypt/decrypt the cookie
   cookie: {
-    maxAge: 1000 * 60 , // how long is the session valid for, in milliseconds
+    maxAge: 1000 * 60 * 60, // how long is the session valid for, in milliseconds
     secure: false, // used over https only, should be true in production
     httpOnly: true, // cannot access the cookie from JS using document.cookie
     // keep this true unless there is a good reason to let JS access the cookie
