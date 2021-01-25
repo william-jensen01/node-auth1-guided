0026a6cb236e3c1708d28cdb06a4c94811e76258
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-25 18:42:31 +0100

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -2,4 +2,12 @@ const express = require("express");
 
 const router = express.Router()
 
+router.post('/register', (req, res) => {
 
+})
+
+router.post('/register', (req, res) => {
+
+})
+
+module.exports = router
