06bf90f2cef6cb903faa9c5841abaaca76d40c5c
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-25 19:32:25 +0100

    wip

--- api/auth/auth-midleware.js
+++ api/auth/auth-midleware.js
@@ -1,3 +1,6 @@
 module.exports = function(req, res, next) {
-  // server maintains a "sessions" list
+  // server maintains a "sessions" list/array
+  // with objects that represent stored sessions
+  // (clients that authed successfully)
+  if (req.session && req.session.user)
 }
