1357533d9f3c225ff8d3256f6204fb7bde6f0fc9
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-25 19:03:29 +0100

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post('/register', (req, res) => {
   const { username, password, role = 2 } = req.body
   // save the username & password
-  bcrypt.hashSync()
+  bcrypt.hashSync(password, 10) // 2 ^ 10
 });
 
 router.post('/login', (req, res) => {
