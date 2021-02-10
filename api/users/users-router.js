2281d4cf1fb52fe8cde30e4b871bc0149ce5e74e
William Jensen <williambjensen01@gmail.com>
2021-02-10 12:33:15 -0700

    completed guided

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
-
 const Users = require("./users-model.js");
+const loginCheck = require('../auth/logged-in-check-middleware');
 
-router.get("/", (req, res) => {
+router.get("/", loginCheck, (req, res) => {
   Users.find()
     .then(users => {
       res.status(200).json(users);
